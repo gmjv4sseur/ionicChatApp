bf9b419ea2583975ba55273f8f37a1f4c41d531e
Guillaume <gmj.vasseur@gmail.com>
2020-01-10 21:17:26 +0100

    initialize weather service

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WeatherService } from '../services/weather.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,35 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
 
+  weatherInfos: any;
+  weatherIcon: any;
+
+  constructor(private weatherService: WeatherService) {
+
+    this.weatherIcon = this.getIcon();
+
+  }
+
+  getWeather(cityname) {
+
+    if (!this.weatherInfos) {
+      this.weatherService.getWeather(cityname).subscribe(res => {
+
+        this.weatherInfos = res;
+        this.weatherIcon = this.getIcon();
+        console.log(this.weatherInfos);
+
+      })
+    }
+  }
+
+  getIcon() {
+    if (this.weatherInfos) {
+      return "https://openweathermap.org/img/wn/" + this.weatherInfos['weather'][0]['icon'] + "@2x.png";
+    }
+    else{
+      return "../../assets/icon/favicon.png";
+    }
+  }
 }
